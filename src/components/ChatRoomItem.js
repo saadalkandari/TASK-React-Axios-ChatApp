ef25d1633f5a28da454c0fb7ea4c4be62be0f963
omsi96 <omsi96@gmail.com>
2022-02-06 16:54:57 +0300

    Renaming ChatRoomItem.js

---
+++ src/components/ChatRoomItem.js
@@ -0,0 +1,42 @@
+import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import UpdateRoomModal from "./UpdateRoomModal";
+
+const ChatRoomItem = ({ room }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => setIsOpen(false);
+
+  const openModal = () => setIsOpen(true);
+
+  const handleDelete = () => {
+    // call a function from app to delete a room (pass room.id as a parameter)
+  };
+  return (
+    <div className="group">
+      <Link to={`/room/${room.slug}`}>
+        <div style={{ animationDelay: "0.1" }} className="chatlist__item">
+          <div className="avatar">
+            <div className="avatar-img">
+              <img src={room.image} alt="#" />
+            </div>
+          </div>
+          <div className="userMeta">
+            <p>{room.title}</p>
+            <span className="activeTime">{room.description}</span>
+          </div>
+        </div>
+      </Link>
+
+      <Button className="delete" onClick={handleDelete}>
+        Delete
+      </Button>
+      <Button className="delete" onClick={openModal}>
+        Update
+      </Button>
+      <UpdateRoomModal isOpen={isOpen} closeModal={closeModal} room={room} />
+    </div>
+  );
+};
+export default ChatRoomItem;
